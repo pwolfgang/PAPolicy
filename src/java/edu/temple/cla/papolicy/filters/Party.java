2cf87547b8754c590f58a75c2c1f30b21e702eb0
Paul Wolfgang <Paul Wolfgang@dfd8a25d-4284-4840-9433-f4ffa13726af>
2010-05-18 17:28:16 +0000

    Added a Party Filter
    git-svn-id: file:///var/svn/PAPolicy/trunk@28 dfd8a25d-4284-4840-9433-f4ffa13726af

---
+++ src/java/edu/temple/cla/papolicy/filters/Party.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.temple.cla.papolicy.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+public class Party extends Filter {
+
+    private static final String BOTH = "587";
+    private String parameterName;
+    private String parameterValue;
+
+    private String filterQueryString;
+    private String filterQualifier;
+
+    public Party(int id, int tableId, String description,
+            String columnName, String tableReference, String additionalParam) {
+        super(id, tableId, description, columnName, tableReference,
+                additionalParam);
+        parameterName = "F" + getId();
+    }
+
+    @Override
+    public String getFilterFormInput() {
+        return ""+getDescription()+"\n"+
+"        <br /><input type=\"radio\" name=\"F"+getId()+"\" value=\""+BOTH+"\" checked=\"checked\" />&nbsp;no filter\n"+
+"              <input type=\"radio\" name=\"F"+getId()+"\" value=\"0\" />&nbsp;Republican\n"+
+"              <input type=\"radio\" name=\"F"+getId()+"\" value=\"1\" />&nbsp;Democrat\n"+
+"              <input type=\"radio\" name=\"F"+getId()+"\" value=\"2\" />&nbsp;Other";
+    }
+
+    public void setFilterParameterValues(HttpServletRequest request) {
+        parameterValue = request.getParameter(parameterName);
+        buildFilterStrings();
+    }
+
+    public String getFilterQueryString() {
+        return filterQueryString;
+    }
+
+    private void buildFilterStrings() {
+        if (BOTH.equals(parameterValue)) {
+            filterQueryString = "";
+            filterQualifier = "";
+        } else if ("0".equals(parameterValue)) { // Republican
+            filterQueryString = getColumnName() + "=0";
+            filterQualifier = "Sponsored by a Republican";
+        } else if ("1".equals(parameterValue)) { // Democrat
+            filterQueryString = getColumnName() + "=1";
+            filterQualifier = "Sponsored by a Democrat";
+        } else if ("2".equals(parameterValue)) { // Third Party
+            filterQueryString = getColumnName() + "=2";
+            filterQualifier = "Sponsored by a Member of a Third Party";
+        }
+    }
+
+    public String getFilterQualifier() {
+        return filterQualifier;
+    }
+}
