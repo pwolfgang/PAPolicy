1608445f0b738dd6a008689c9ace17ce2fc44d26
Paul Wolfgang <Paul Wolfgang@dfd8a25d-4284-4840-9433-f4ffa13726af>
2010-06-01 19:56:15 +0000

    Added download of Governor's budget address
    git-svn-id: file:///var/svn/PAPolicy/trunk@32 dfd8a25d-4284-4840-9433-f4ffa13726af

---
+++ src/java/edu/temple/cla/papolicy/controllers/DownloadGovernorsBudgetAddress.java
@@ -0,0 +1,118 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.temple.cla.papolicy.controllers;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+public class DownloadGovernorsBudgetAddress extends HttpServlet {
+
+    private String path;
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        OutputStream out = null;
+        String fileName = request.getParameter("year");
+        try {
+            File pathFile = new File(path);
+            File inputFile = new File(path, fileName +
+                    " Governors Budget Address.pdf");
+            InputStream in = new BufferedInputStream(
+                    new FileInputStream(inputFile));
+            response.setContentType("application/pdf");
+            out = response.getOutputStream();
+            int c;
+            while ((c = in.read()) != -1) out.write(c);
+            in.close();
+        } catch (FileNotFoundException ex) {
+            try {
+            response.setContentType("text/html");
+            PrintWriter pw = response.getWriter();
+            pw.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"");
+            pw.println("\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+            pw.println("<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">");
+            pw.println("<head>");
+            pw.println("<title>Error Page</title>");
+            pw.println("</head>");
+            pw.println("<body>");
+            pw.println("<img src=\"images/cla_201_4c.jpg\" alt=\"CLA Logo\" width=\"592\" height=\"77\" />");
+            pw.println("<h1>Pennsylvania Policy Database Project</h1>");
+            pw.println("<p>We are sorry, but " + fileName + " is not available.</p>");
+            pw.println("</body>");
+            pw.println("</html>");
+            pw.close();
+            }catch (Throwable t) {
+                String message = t.toString();
+            }
+        } finally {
+            if (out != null)
+                out.close();
+        }
+    } 
+
+    @Override
+    public void init() throws ServletException {
+        path = getServletConfig().getInitParameter("path");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
