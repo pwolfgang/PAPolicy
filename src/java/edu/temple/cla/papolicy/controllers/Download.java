af73aa3bc087ee21f999182337646b33bfb26365
Paul Wolfgang <Paul Wolfgang@dfd8a25d-4284-4840-9433-f4ffa13726af>
2010-10-15 20:06:58 +0000

    Improved download of excel.
    git-svn-id: file:///var/svn/PAPolicy/trunk@48 dfd8a25d-4284-4840-9433-f4ffa13726af

--- src/java/edu/temple/cla/papolicy/controllers/Download.java
+++ src/java/edu/temple/cla/papolicy/controllers/Download.java
@@ -5,16 +5,26 @@
 
 package edu.temple.cla.papolicy.controllers;
 
+import java.io.OutputStream;
+import java.util.Date;
 import edu.temple.cla.papolicy.Utility;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import static java.sql.Types.*;
 
 /**
  *
@@ -22,7 +32,7 @@ import org.springframework.web.servlet.mvc.AbstractController;
  */
 public class Download extends AbstractController{
 
-    private DataSource datasource;
+    private DataSource dataSource;
 
 
     /**
@@ -35,10 +45,80 @@ public class Download extends AbstractController{
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
+        ResultSetMetaData rsmd = null;
         try {
-            conn = datasource.getConnection();
+            conn = dataSource.getConnection();
             stmt = conn.createStatement();
             rs = stmt.executeQuery(query);
+            rsmd = rs.getMetaData();
+            int numColumns = rsmd.getColumnCount();
+            String[] columnNames = new String[numColumns];
+            int[] columnTypes = new int[numColumns];
+            for (int i = 0; i < numColumns; i++) {
+                columnNames[i] = rsmd.getColumnName(i+1);
+                columnTypes[i] = rsmd.getColumnType(i+1);
+            }
+            Workbook wb = new HSSFWorkbook();
+            Sheet sheet = wb.createSheet("sheet 1");
+            CreationHelper helper = wb.getCreationHelper();
+            int rowNum = 0;
+            Row row = sheet.createRow(rowNum);
+            for (int i = 0; i < numColumns; i++) {
+                Cell cell = row.createCell(i);
+                cell.setCellValue(helper.createRichTextString(columnNames[i]));
+            }
+            while (rs.next()) {
+                rowNum++;
+                row = sheet.createRow(rowNum);
+                for (int i = 0; i < numColumns; i++) {
+                    Cell cell = row.createCell(i);
+                    switch (columnTypes[i]) {
+                        case BIT:
+                        case TINYINT:
+                        case SMALLINT:
+                        case INTEGER:
+                            int intValue = rs.getInt(i+1);
+                            if (!rs.wasNull()) {
+                                cell.setCellValue(intValue);
+                            }
+                            break;
+                        case BIGINT:
+                            long longValue = rs.getLong(i+1);
+                            if (!rs.wasNull()) {
+                                cell.setCellValue(longValue);
+                            }
+                            break;
+                        case FLOAT:
+                        case REAL:
+                        case DOUBLE:
+                        case NUMERIC:
+                        case DECIMAL:
+                            double doubleValue = rs.getDouble(i+1);
+                            if (!rs.wasNull()) {
+                                cell.setCellValue(doubleValue);
+                            }
+                            break;
+                        case CHAR:
+                        case VARCHAR:
+                        case LONGVARCHAR:
+                            String stringValue = rs.getString(i+1);
+                            if (!rs.wasNull()) {
+                                cell.setCellValue(helper.createRichTextString(stringValue));
+                            }
+                            break;
+                        case DATE:
+                        case TIME:
+                        case TIMESTAMP:
+                             Date dateValue = rs.getDate(i+1);
+                             if (!rs.wasNull()) {
+                                cell.setCellValue(dateValue);
+                            }
+                    }
+                }
+            }
+            OutputStream out = response.getOutputStream();
+            wb.write(out);
+            out.close();
         } catch (SQLException ex) {
             
         } finally {
@@ -52,9 +132,10 @@ public class Download extends AbstractController{
     /**
      * @param datasource the datasource to set
      */
-    public void setDatasource(DataSource datasource) {
-        this.datasource = datasource;
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
+    public DataSource getDataSource() {return dataSource;}
 
 }
