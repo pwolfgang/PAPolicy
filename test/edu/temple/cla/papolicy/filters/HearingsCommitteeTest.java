a9e874a7e648a7a7dcebfc99446a65e1567d4af2
Paul <Paul@Paul-PC.home>
2013-02-24 12:50:26 -0500

    Test for HearingsCommittee. (Note this is only a regression test of the filter form input.)

---
+++ test/edu/temple/cla/papolicy/filters/HearingsCommitteeTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.temple.cla.papolicy.filters;
+
+import edu.temple.cla.papolicy.dao.CommitteeAlias;
+import edu.temple.cla.papolicy.dao.CommitteeAliasMapper;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+public class HearingsCommitteeTest {
+
+    HearingsCommittee filter;
+    @Mocked
+    HttpServletRequest request;
+    @Mocked
+    SimpleJdbcTemplate jdbcTemplate;
+
+    public HearingsCommitteeTest() {
+    }
+
+    @Before
+    public void setUp() {
+        filter = new HearingsCommittee(102, 5, "Senate Committee", null, "CommitteeAliases", "Senate");
+        new NonStrictExpectations() {
+            {
+                CommitteeAlias committee = new CommitteeAlias();
+                committee.setID(39);
+                committee.setCtyCode(201);
+                committee.setAlternateName("Aging and Youth");
+                committee.setName("Aging and Youth");
+                committee.setStartYear(0);
+                committee.setEndYear(9999);
+                jdbcTemplate.query(anyString, new CommitteeAliasMapper());
+                result = Arrays.asList(new CommitteeAlias[]{committee});
+            }
+        };
+        filter.setJdbcTemplate(jdbcTemplate);
+    }
+
+    @Test
+    public void testGetFilterFormInput() {
+        String expected = "Senate Hearings\n" +
+"                <br /><select name=\"F102>\"\n" +
+"                <option value=\"ALL\" selected=\"selected\">ALL COMMITTEES</option><option value=\"201\">Aging and Youth</option></select>";
+        assertEquals(expected, filter.getFilterFormInput());
+    }
+
+    @Test
+    public void testGetFilterQueryString() {
+        String expected = "";
+        String result = filter.getFilterQueryString();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetFilterQualifier() {
+        String expected = "";
+        String result = filter.getFilterQualifier();
+        assertEquals(expected, result);
+    }
+
+}
