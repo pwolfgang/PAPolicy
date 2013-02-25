5b1576eef4e1da5cf10378505b7588ec6a859f19
Paul Wolfgang <Paul Wolfgang@wh1041>
2013-02-25 12:37:36 -0500

    Regression test for PubicOpinionFilters.

---
+++ test/edu/temple/cla/papolicy/filters/PublicOpinionFiltersTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.temple.cla.papolicy.filters;
+
+import javax.servlet.http.HttpServletRequest;
+import mockit.Expectations;
+import mockit.Mocked;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+public class PublicOpinionFiltersTest {
+    
+    private PublicOpinionFilters filter;
+    @Mocked
+    HttpServletRequest request;
+    
+    public PublicOpinionFiltersTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        filter = new PublicOpinionFilters(901, 10, null, null, null, null);
+    }
+
+    @Test
+    public void testGetFilterFormInput() {
+        System.out.println("getFilterFormInput");
+        String expResult = "Display as <input type=\"radio\" name=\"mipdisp\" value=\"0\" checked=\"checked\" />Percent\n" +
+"                <input type=\"radio\" name=\"mipdisp\" value=\"1\" />Rank";
+        String result = filter.getFilterFormInput();
+        assertEquals(expResult, result);
+    }
+
+
+    @Test
+    public void testGetFilterQueryString() {
+        System.out.println("getFilterQueryString");
+        PublicOpinionFilters instance = filter;
+        String expResult = "";
+        String result = instance.getFilterQueryString();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetFilterQualifier() {
+        System.out.println("getFilterQualifier");
+        PublicOpinionFilters instance = filter;
+        String expResult = "";
+        String result = instance.getFilterQualifier();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetMipdispZero() {
+        System.out.println("getMipdisp");
+        PublicOpinionFilters instance = filter;
+        setParameterValue("0");
+        String expResult = "0";
+        String result = instance.getMipdisp();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetMipdispOne() {
+        System.out.println("getMipdisp");
+        PublicOpinionFilters instance = filter;
+        setParameterValue("1");
+        String expResult = "1";
+        String result = instance.getMipdisp();
+        assertEquals(expResult, result);
+    }
+
+    private void setParameterValue(final String value) {
+        new Expectations() {
+            {
+                request.getParameter("mipdisp");
+                result = value;
+            }
+        };
+        filter.setFilterParameterValues(request);
+    }
+}
