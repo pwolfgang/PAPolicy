ac6742b26b861edf054b2bb6054e3df975d9cea5
Paul Wolfgang <Paul Wolfgang@wh1041>
2013-03-08 16:26:32 -0500

    Added new ColumnTest (shell)

---
+++ test/edu/temple/cla/papolicy/ColumnTest.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.temple.cla.papolicy;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+public class ColumnTest {
+    
+    public ColumnTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testGetDownloadTitle() {
+    }
+
+    @Test
+    public void testGetDownloadURL() {
+    }
+
+    @Test
+    public void testGetFilteredTotalQueryString_0args() {
+    }
+
+    @Test
+    public void testGetUnfilteredTotalQueryString_0args() {
+    }
+
+    @Test
+    public void testGetUnfilteredTotalQueryString_int_int() {
+    }
+
+    @Test
+    public void testGetFilteredTotalQueryString_int_int() {
+    }
+
+    @Test
+    public void testGetTopicCountQueryString_0args() {
+    }
+
+    @Test
+    public void testGetTopicCountQueryString_int_int() {
+    }
+
+    @Test
+    public void testGetUnits() {
+    }
+
+    @Test
+    public void testGetValue() {
+    }
+
+    @Test
+    public void testGetPercentOfTotal() {
+    }
+
+    @Test
+    public void testGetPercent() {
+    }
+
+    @Test
+    public void testSetInitialPrevValue() {
+    }
+
+    @Test
+    public void testGetPercentChange() {
+    }
+
+    @Test
+    public void testSetValueMap() {
+    }
+
+    @Test
+    public void testSetUnfilteredTotalMap() {
+    }
+
+    @Test
+    public void testSetFilteredTotalMap() {
+    }
+
+    @Test
+    public void testSetDisplayedValue() {
+    }
+
+    @Test
+    public void testSetDrillDown() {
+    }
+
+    @Test
+    public void testGetDisplayedValue() {
+    }
+
+    @Test
+    public void testGetDisplayedValueString() {
+    }
+
+    @Test
+    public void testGetDrillDown() {
+    }
+
+    @Test
+    public void testGetRowKeys() {
+    }
+
+    @Test
+    public void testGetAxisTitle() {
+    }
+
+    @Test
+    public void testGetTable() {
+    }
+
+    @Test
+    public void testGetDownloadQuery() {
+    }
+
+    @Test
+    public void testSetDownloadQuery() {
+    }
+
+    @Test
+    public void testGetMinValue() {
+    }
+
+    @Test
+    public void testGetMaxValue() {
+    }
+
+    @Test
+    public void testParseFreeText() {
+    }
+}
