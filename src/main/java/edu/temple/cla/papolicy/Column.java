ec4647a9b06378c32f8b1cabab7b4fdfa89e5453
Paul Wolfgang <wolfgang@temple.edu>
2018-09-10 11:46:44 -0400

    Fixed inadvertent changes to license.

--- src/main/java/edu/temple/cla/papolicy/Column.java
+++ src/main/java/edu/temple/cla/papolicy/Column.java
@@ -6,9 +6,9 @@
  * modification, are permitted provided that the following conditions are met:
  *
  * * Redistributions of source code must retain the above copyright notice, this
- *   yearValueList of conditions and the following disclaimer.
+ *   list of conditions and the following disclaimer.
  * * Redistributions in binary form must reproduce the above copyright notice,
- *   this yearValueList of conditions and the following disclaimer in the documentation
+ *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
  * * All advertising materials features or use of this software must display 
  *   the following  acknowledgement
